8637284a1d3d919e938d0f808233f163179dec94
Sonia-sahu <ss1575650@gmail.com>
2025-09-10 09:14:01 +0000

    changed logic

--- main.py
+++ main.py
@@ -6,3 +6,16 @@ if number % 2 == 0:
     print("The number is even.")
 else:
     print("The number is odd.")
+def check_even_odd(number):
+    if number % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+
+if __name__ == "__main__":
+    try:
+        num = int(input("Enter a number: "))
+        result = check_even_odd(num)
+        print(f"The number {num} is {result}.")
+    except ValueError:
+        print("Please enter a valid integer.")
